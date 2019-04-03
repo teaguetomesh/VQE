a8b24bedfe6b554073b48e8c3776ab298a6fc667
Teague Tomesh <ttomesh@princeton.edu>
2019-04-03 17:54:36 -0400

    getting stdout from subprocesses

--- gen_vqe.py
+++ gen_vqe.py
@@ -18,16 +18,14 @@ def main(argv):
     argv contains the arguments to use when calling main.py as well as an int indicating the
     number of trials to run.
     '''
-    num_args = 7
+    num_args = 8
 
     if len(argv) != num_args:
         print('\n---ERROR---')
         print('Must pass {} arguments to gen_vqe.py:'.format(num_args))
         print('  <hamiltonian> <referenceState> <ansatz> <numQubits> <optimizer>',
-              '<output> <numIterations>\n')
+              '<output> <terminal_output> <numIterations>\n')
         sys.exit(2)
-
-    print(argv)
 
     hamPath   = argv[0]
     refState  = argv[1]
@@ -35,12 +33,19 @@ def main(argv):
     numQ      = argv[3]
     optimizer = argv[4]
     outPath   = argv[5]
-    num_iters = int(argv[6])
+    term_output = argv[6]
+    num_iters = int(argv[7])
 
     for i in range(num_iters):
-        outPath += str(i)
-        subprocess.run(['./main.py','-p',hamPath,'-r',refState,'-a',ansatz,
-            '-q',numQ,'-o',optimizer,'-t',outPath])
+        term_output_file = 'Results/'+term_output+'{}.txt'.format(i)
+        print('Current Iteration: {}'.format(i))
+        print('---starting subprocess---')
+        with open(term_output_file,'w') as tf:
+            outputPath = outPath + str(i)
+            subprocess.run(['./main.py','-p',hamPath,'-r',refState,
+                       '-a',ansatz,'-q',numQ,'-o',optimizer,'-t',outputPath],
+                       stdout=tf)
+        print('Finished subprocess {}'.format(i))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
